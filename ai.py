8f654c7ddbc645f6fb651e06530e725b14a51b76
Shashank B N <bnshashankr1597@gmail.com>
2023-10-05 20:52:36 +0530

    feat: Added prodiciton and venv

--- ai.py
+++ ai.py
@@ -1,5 +1,24 @@
-#!./venv/bin/python
+import subprocess
+import platform
 
+venv_name = "myenv"
+log_file = open("output.log", "w")
+
+subprocess.run(["python","-m","venv",venv_name], stdout=log_file, stderr=subprocess.STDOUT, check=True)
+
+os = platform.system()
+
+if os == "Windows":
+    if subprocess.run([venv_name + "\\Scripts\\activate"], stdout=log_file, stderr=subprocess.STDOUT, shell=True).returncode != 0:
+        raise Exception("Failed to activate virtual environment on Windows")
+    subprocess.run([venv_name + "\\Scripts\\pip", "install", "xgboost"], stdout=log_file, stderr=subprocess.STDOUT,check=True)
+    subprocess.run([venv_name + "\\Scripts\\pip", "install", "scikit-learn"], stdout=log_file, stderr=subprocess.STDOUT, check=True)
+    subprocess.run([venv_name + "\\Scripts\\pip", "install", "yfinance"], stdout=log_file, stderr=subprocess.STDOUT, check=True)
+else:
+    subprocess.run(["source", venv_name + "/bin/activate"], stdout=log_file, stderr=subprocess.STDOUT, shell=True)
+    subprocess.run([venv_name + "/bin/pip", "install", "xgboost"], stdout=log_file, stderr=subprocess.STDOUT, check=True)
+    subprocess.run([venv_name + "/bin/pip", "install", "scikit-learn"], stdout=log_file, stderr=subprocess.STDOUT, check=True)
+    subprocess.run([venv_name + "/bin/pip", "install", "yfinance"], stdout=log_file, stderr=subprocess.STDOUT, check=True)
 import warnings
 
 warnings.filterwarnings("ignore",category=DeprecationWarning)
